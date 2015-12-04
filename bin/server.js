21d1cad1446c1801bab184d566a73fb3af7c4528
siwei.ransw <siwei.ransw@alibaba-inc.com>
2015-12-04 15:59:29 +0800

    bug fix

--- bin/server.js
+++ bin/server.js
@@ -5,11 +5,13 @@ var path = require('path'),
     recluster = require('recluster');
 
 var cluster = recluster(path.join(__dirname, 'app.js'));
+
+
 cluster.run();
 
 process.on('SIGUSR2', function () {
-  // logger.warn('Got SIGUSR2, reloading cluster...');
+ 	console.log('Got SIGUSR2, reloading cluster...');
     cluster.reload();
 });
 
-//logger.warn('spawned cluster, kill -s SIGUSR2 ' + process.pid + ' to reload');
+console.log('spawned cluster, kill -s SIGUSR2 ' + process.pid + ' to reload');
